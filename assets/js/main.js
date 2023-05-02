02abf50ac3c57a2f866ec38b2eed68f90e64d0ce
aytacallahverdiyeva <aytac.allahverdiyeva@gmail.com>
2023-05-02 15:36:43 +0400

    some commit

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+//!===============Random Task with button and box that changes own colors
+//* Variables for button & box
+// let button = document.querySelector ('button');
+// let box = document.getElementById ('changeMe');
+
+//* Function for changing colors (rainbow) of the box with conditional statement (if/else)
+// function changeColor () {
+// 	if (box.style.background == 'red') {
+// 		box.style.background = 'orangered'
+// 	} 
+// 	else if (box.style.background == 'orangered') {
+// 		box.style.background = 'yellow'
+// 	}
+// 	else if (box.style.background == 'yellow') {
+// 		box.style.background = 'green'
+// 	}
+// 	else if (box.style.background == 'green') {
+// 		box.style.background = 'lightseagreen'
+// 	}
+// 	else if (box.style.background == 'lightseagreen') {
+// 		box.style.background = 'blue'
+// 	}
+// 	else if (box.style.background == 'blue') {
+// 		box.style.background = 'purple'
+// 	}
+// 	else {
+// 		box.style.background = 'red'
+// 	}
+// }
+
+
+//!==================Task calc with two numbers (+ - * /)===================
+// const operator = prompt("Choose one of this operators (+ - / *)");
+
+//* Prompts for enter two any numbers
+// const number1 = parseFloat(prompt("Enter here first number"));
+// const number2 = parseFloat(prompt("Enter here second number"));
+
+// let result;
+
+//* Operators performing their functions respectively 
+// if (operator == '+') {
+// 	result = number1 + number2
+// }
+// else if (operator == '-') {
+// 	result = number1 - number2
+// }
+// else if (operator == '*') {
+// 	result = number1 * number2
+// }
+// else {
+// 	result = number1 / number2
+// }
+
+//* Result/Output/Console
+// console.log(`${number1} ${operator} ${number2} = ${result}`);
+
+
+//!=========================Task 3 Reduce in array which has three objects :( ======
+// const persons = [
+// 	{name:"Farid", salary:150},
+// 	{name:"Aytac", salary:250},
+// 	{name:"Tergul", salary:350},
+// ]
+// const sumSalary = persons.reduce((prev,next) => {
+// 	return prev.salary + next.salary;
+// }, 0);
+// console.log(sumSalary);
